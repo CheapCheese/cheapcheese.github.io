6762228de9e78cf0b5d818adf115ecb817516c99
Alexfp28 <alexlopezdelafuente@gmail.com>
2024-06-07 15:51:26 +0200

    Desarrollo del Front - Cambios en las peticiones HTTPS - HTTP

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,24 +1,24 @@
 import axios from 'axios';
-axios.defaults.headers.common['Origin'] = 'https://20.224.64.191:8080';
+axios.defaults.headers.common['Origin'] = 'http://20.224.64.191:8080';
 
 class UserService {
 
     getAllUsers() {
         return axios.get(
-            'https://20.224.64.191:8080/api/v1/users/getAllUsers'
+            'http://20.224.64.191:8080/api/v1/users/getAllUsers'
         );
     }
 
     saveUser(data) {
         return axios.post(
-            'https://20.224.64.191:8080/api/v1/users/saveUser',
+            'http://20.224.64.191:8080/api/v1/users/saveUser',
             data
         )
     }
 
     deleteUser(data) {
         return axios.post(
-            'https://20.224.64.191:8080/api/v1/users/deleteUser',
+            'http://20.224.64.191:8080/api/v1/users/deleteUser',
             data
         )
     }
